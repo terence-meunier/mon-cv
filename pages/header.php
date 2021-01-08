d5d5d8867bbeefb595fd43e0c6fb4a17a5feea9c
Térence Meunier <terence.meunier@gmail.com>
2021-01-08 22:47:56 +0100

    build(core): deplace pages files in the pages directory

---
+++ pages/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Térence Meunier">
+    <meta name="description" content="<?= $metaDescription ?>">
+    <link rel="stylesheet" href="../styles/styles.css" type="text/css">
+    <title>Térence MEUNIER - <?= $metaTitle ?></title>
+</head>
+
+<body>
+<header>
+    <p><a href="../index.php?lenomdelapage='profil'" class="link_header">Térence MEUNIER<br>Infographiste | Dev Web | Dessinateur</a></p>
+    <img src="../images/header/profil.jpg" alt="Photo de profil">
+</header>
+
+<nav id="main_menu" class="main_style">
+    <ul>
+        <li><a href="../index.php?page=profil" class="link_main_menu">Mon Profil</a></li>
+        <li><a href="../index.php?page=hobby" class="link_main_menu">Mon Hobby</a></li>
+        <li><a href="../index.php?page=contact" class="link_main_menu">Contact</a></li>
+    </ul>
+</nav>
