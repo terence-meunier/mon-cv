cab2e9767cbc7d3926fb5db713262b1e2f574cb3
Térence MEUNIER <terence.meunier@gmail.com>
2021-01-08 15:39:15 +0100

    build(core): Add PHP files

---
+++ footer.php
@@ -0,0 +1,30 @@
+<footer class="footer_style">
+    <nav id="secondary_menu">
+        <h3>Plan du site</h3>
+        <ul>
+            <li><a href="index.html" class="link_secondary_menu">Mon Profil</a></li>
+            <li><a href="hobby.html" class="link_secondary_menu">Mon Hobby</a></li>
+            <li><a href="contact.html" class="link_secondary_menu">Contact</a></li>
+        </ul>
+    </nav>
+
+    <nav id="social_menu">
+        <h3>Liens Sociaux</h3>
+        <ul>
+            <li><a href=""><img src="images/social_icons/facebook.png" width=50 height=50 alt="Lien facebook"></a>
+            </li>
+            <li><a href=""><img src="images/social_icons/instagram.png" width=50 height=50 alt="Lien instagram"></a>
+            </li>
+            <li><a href=""><img src="images/social_icons/linked.png" width=50 height=50 alt="Lien linkedIn"></a>
+            </li>
+            <li><a href=""><img src="images/social_icons/snapchat.png" width=50 height=50 alt="Lien snap"></a></li>
+        </ul>
+    </nav>
+
+    <p>
+        Ce site est développé et maintenu par Térence MEUNIER - Futur Dev'Web
+        <br>
+        <br>
+        Tous droits réservés - @copyright 2020 - version 1.0
+    </p>
+</footer>
---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <p><a href="index.html" class="link_header">Térence MEUNIER<br>Infographiste | Dev Web | Dessinateur</a></p>
+    <img src="images/header/profil.jpg" alt="Photo de profil">
+</header>
+
+<nav id="main_menu" class="main_style">
+    <ul>
+        <li><a href="index.html" class="link_main_menu">Mon Profil</a></li>
+        <li><a href="hobby.html" class="link_main_menu">Mon Hobby</a></li>
+        <li><a href="contact.html" class="link_main_menu">Contact</a></li>
+    </ul>
+</nav>
