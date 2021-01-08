7e654b0eeaf7e80215f8590e61b1cd331c807cd5
Térence MEUNIER <terence.meunier@gmail.com>
2021-01-08 16:19:27 +0100

    fix(header): add header in index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="Térence Meunier">
-    <meta name="description" content="Cette page sert de portail pour la présentation de mon CV.
-    Vous y trouverez mes compétences, mes formations, mon parcours professionnel, mes hobbies.">
-    <link rel="stylesheet" href="styles/styles.css" type="text/css">
-    <title>Térence MEUNIER - Mon Profil</title>
-</head>
-
-<body>
 <!-- On ajoute le header à la page -->
 <?php include 'header.php' ?>
 <main>
