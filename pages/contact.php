08a0c5c0ba9d3053ae0fc289b351f3948d18570d
Térence MEUNIER <terence.meunier@gmail.com>
2021-01-12 12:22:05 +0100

    feat(contact): Update form

--- pages/contact.php
+++ pages/contact.php
@@ -1,61 +1,47 @@
 <?php
-// Traitement du formulaire
-// Tableau de filtres
-$args = [
-    'gender' => FILTER_SANITIZE_STRING,
-    'contact_lastname' => FILTER_SANITIZE_STRING,
-    'contact_firstname' => FILTER_SANITIZE_STRING,
-    'contact_email' => FILTER_SANITIZE_EMAIL,
-    'contact_subject' => FILTER_SANITIZE_STRING,
-    'message_text' => FILTER_SANITIZE_STRING
-];
+// Testé si le formulaire à été envoyé
+$formSent = filter_has_var(INPUT_POST, 'gender');
 
-// Application des filtres sur POST
-$datas = filter_input_array(INPUT_POST, $args);
+if ($formSent) {
+    // Nettoyage des données réceptionnées
+    $args = [
+        'gender' => FILTER_SANITIZE_STRING,
+        'contact_lastname' => FILTER_SANITIZE_STRING,
+        'contact_firstname' => FILTER_SANITIZE_STRING,
+        'contact_email' => FILTER_SANITIZE_EMAIL,
+        'contact_subject' => FILTER_SANITIZE_STRING,
+        'message_text' => FILTER_SANITIZE_STRING
+    ];
 
-// Test si les champ sont vide
-// Champ Nom
-if (isset($datas['contact_lastname']) && empty($datas['contact_lastname'])) {
-    $lastnameIsEmpty = true;
-} else {
-    $lastnameIsEmpty = false;
-}
-// Champ Prénom
-if (isset($datas['contact_firstname']) && empty($datas['contact_firstname'])) {
-    $firstnameIsEmpty = true;
-} else {
-    $firstnameIsEmpty = false;
-}
-// Champ email
-if (isset($datas['contact_email']) && empty($datas['contact_email'])) {
-    $emailIsEmpty = true;
-} else {
-    $emailIsEmpty = false;
-}
-// Champ message
-if (isset($datas['message_text']) && empty($datas['message_text'])) {
-    $messageIsEmpty = true;
-} else {
-    $messageIsEmpty = false;
-}
+    $datas = filter_input_array(INPUT_POST, $args);
 
-// Verification de l'adresse email valide
-if (isset($datas['contact_email']) && filter_var($datas['contact_email'], FILTER_VALIDATE_EMAIL)) {
-    $emailIsValid = true;
-} else {
-    $emailIsValid = false;
-}
-
-// Enregistrement du fichier
-if ($datas && !$lastnameIsEmpty && !$firstnameIsEmpty && !$emailIsEmpty && !$messageIsEmpty && $emailIsValid) {
-    // Création du nom du fichier
-    $file = 'contact/contact_' . date('Y-m-d-H-i-s') . '.txt';
-
-    // Ajout des données au fichier et gestion d'une variable d'information si le message est enregistré
-    if (file_put_contents($file, implode(' | ', $datas))) {
-        $info_msg = "Votre message est enregistré";
+    // Testé si les champs sont renseignés (champs vides)
+        // Champ nom
+        $lastnameIsEmpty = empty($datas['contact_lastname']);
+        // Champ prénom
+        $firstnameIsEmpty = empty($datas['contact_firstname']);
+        // Champ email
+        $emailIsEmpty = empty($datas['contact_email']);
+        // Champ message
+        $messageIsEmpty = empty($datas['message_text']);
+    // Verifier si l'adresse email est valide
+    $datas['contact_email'] = filter_var($datas['contact_email'], FILTER_VALIDATE_EMAIL);
+    if ($datas['contact_email']) {
+        $emailIsValid = true;
     } else {
-        $info_msg = "Erreur d'enregistrement du message";
+        $emailIsValid = false;
+    }
+    // Verifier si le message fait au moins 5 caractères
+    $min5car = strlen($datas['contact_lastname']) >= 5;
+    // Verifier si le genre renseigné est "Mr" ou "Mme"
+    $genderIsValid = $datas['gender'] === "Mr" || $datas['gender'] === "Mme";
+    // Verifier si le sujet renseigné est "proposition emploi" ou "demande information et prestations"
+    $subjectIsValid = $datas['contact_subject'] === "proposition emploi" || $datas['contact_subject'] === "demande information et prestations";
+    // Enregistrement dans le fichier texte dans le dossier /contact sous la forme "contact_2020-01-12-09-52-00.txt"
+    if (!$lastnameIsEmpty && !$firstnameIsEmpty && !$emailIsEmpty && !$messageIsEmpty && $emailIsValid && $min5car && $genderIsValid && $subjectIsValid) {
+        // Envoyer les données dans le fichier texte
+    } else {
+        // Une des données n'est pas valide
     }
 }
 ?>
@@ -67,11 +53,8 @@ if ($datas && !$lastnameIsEmpty && !$firstnameIsEmpty && !$emailIsEmpty && !$mes
         <h2 class="main_style">Vous pouvez me contactez par mail ou par le biais du formulaire de contact</h2>
         <div id="content">
             <article class="main_style transparence">
-                <p><?php if (isset($info_msg)) {
-                        echo $info_msg;
-                    } ?></p>
                 <h3 class="sub_title">Formulaire de Contact</h3>
-                <form action="index.php?page=contact" method="POST">
+                <form action="?page=contact" method="POST">
                     <div id="select_list">
                         <label for="gender">Genre : </label>
                         <select name="gender" id="gender">
@@ -79,38 +62,33 @@ if ($datas && !$lastnameIsEmpty && !$firstnameIsEmpty && !$emailIsEmpty && !$mes
                             <option value="Mme">Madame</option>
                         </select>
                     </div>
-                    <p class="error_message"><?php if($lastnameIsEmpty) { echo 'Champ nom vide'; } ?></p>
                     <div class="champs">
                         <label for="contact_lastname">Votre Nom : </label>
                         <input type="text" id="contact_lastname" name="contact_lastname"
-                               placeholder="Tapez votre nom ici..." value="<?= $datas['contact_lastname'] ?>">
+                               placeholder="Tapez votre nom ici...">
                     </div>
-                    <p class="error_message"><?php if($firstnameIsEmpty) { echo 'Champ prénom vide'; } ?></p>
                     <div class="champs">
                         <label for="contact_firstname">Votre Prénom : </label>
                         <input type="text" id="contact_firstname" name="contact_firstname"
-                               placeholder="Tapez votre Prénom..." value="<?= $datas['contact_firstname'] ?>">
+                               placeholder="Tapez votre Prénom...">
                     </div>
-                    <p class="error_message"><?php if($emailIsEmpty) { echo 'Champ email vide'; } ?></p>
-                    <p class="error_message"><?php if(isset($datas['contact_email']) && !$emailIsValid) { echo 'Email non valide'; } ?></p>
                     <div class="champs">
                         <label for="contact_email">Votre adresse mail : </label>
                         <input type="text" id="contact_email" name="contact_email"
-                               placeholder="Tapez votre adresse email ici..." value="<?= $datas['contact_email'] ?>">
+                               placeholder="Tapez votre adresse email ici...">
                     </div>
                     <div id="radio_buttons">
                         <label>raison du contact :</label>
-                        <input type="radio" id="emploi" name="contact_subject" value="proposition d'emploi" checked>
+                        <input type="radio" id="emploi" name="contact_subject" value="proposition emploi" checked>
                         <label for="emploi">Proposition d'emploi</label>
                         <input type="radio" id="info_prestations" name="contact_subject"
-                               value="demande d'information et prestations">
+                               value="demande information et prestations">
                         <label for="info_prestations">Demande d'information et prestations</label>
                     </div>
-                    <p class="error_message"><?php if($messageIsEmpty) { echo 'Champ message vide'; } ?></p>
                     <div id="message">
                         <label for="message_text">Votre message :</label>
                         <textarea name="message_text" id="message_text" cols="50"
-                                  rows="10" placeholder="Tapez votre message ici..."><?= $datas['message_text'] ?></textarea>
+                                  rows="10" placeholder="Tapez votre message ici..."></textarea>
                     </div>
                     <div id="buttons">
                         <button type="submit">Envoyer le formulaire</button>
