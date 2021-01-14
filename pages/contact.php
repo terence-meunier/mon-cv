c544dede72bee0a9cdd9dc8afd5bb72be9198f23
Térence MEUNIER <terence.meunier@gmail.com>
2021-01-14 12:06:20 +0100

    feat(contact): Refactor code

--- pages/contact.php
+++ pages/contact.php
@@ -3,12 +3,30 @@
 $formSent = filter_has_var(INPUT_POST, 'gender')
     && filter_has_var(INPUT_POST, 'contact_lastname')
     && filter_has_var(INPUT_POST, 'contact_firstname')
-    && filter_has_var(INPUT_POST,'contact_email')
-    && filter_has_var(INPUT_POST,'contact_subject')
-    && filter_has_var(INPUT_POST,'message_text');
+    && filter_has_var(INPUT_POST, 'contact_email')
+    && filter_has_var(INPUT_POST, 'contact_subject')
+    && filter_has_var(INPUT_POST, 'message_text');
 
 // Si le formulaire à bien été envoyé avec tous les champs nécessaire, on fait le traitement
 if ($formSent) {
+
+    // Tableau des messages d'erreurs
+    $formErrors = [
+        'gender' => false,
+        'contact_lastname' => false,
+        'contact_firstname' => false,
+        'contact_email' => false,
+        'contact_email_valid' => false,
+        'contact_subject' => false,
+        'message_text' => false,
+        'min5car' => false
+    ];
+
+    // Tableau des messages d'informations
+    $formInfos = [
+        'msg_info' => ''
+    ];
+
     // Nettoyage des données réceptionnées
     $args = [
         'gender' => FILTER_SANITIZE_STRING,
@@ -28,50 +46,86 @@ if ($formSent) {
 
     // Testé si les champs sont renseignés (champs vides)
     // Champ nom
-    $isLastnameEmpty = empty($datas['contact_lastname']);
+    if (empty($datas['contact_lastname'])) {
+        $formErrors['contact_lastname'] = 'Le champ nom est vide';
+    } else {
+        $formErrors['contact_lastname'] = false;
+    }
     // Champ prénom
-    $isFirstnameEmpty = empty($datas['contact_firstname']);
+    if (empty($datas['contact_firstname'])) {
+        $formErrors['contact_firstname'] = 'Le champ nom est vide';
+    } else {
+        $formErrors['contact_firstname'] = false;
+    }
     // Champ email
-    $isEmailEmpty = empty($datas['contact_email']);
+    if (empty($datas['contact_email'])) {
+        $formErrors['contact_email'] = 'Le champ nom est vide';
+    } else {
+        $formErrors['contact_email'] = false;
+    }
     // Champ message
-    $isMessageEmpty = empty($datas['message_text']);
+    if (empty($datas['message_text'])) {
+        $formErrors['message_text'] = 'Le champ nom est vide';
+    } else {
+        $formErrors['message_text'] = false;
+    }
 
     // Verifier si l'adresse email est valide
     if ($datas['contact_email']) {
-        $isEmailValid = true;
+        $formErrors['contact_email_valid'] = false;
     } else {
-        $isEmailValid = false;
+        $formErrors['contact_email_valid'] = 'Email non valide';
     }
 
     // Verifier si le message fait au moins 5 caractères
-    $isMin5car = strlen($datas['message_text']) >= 5;
+    if (strlen($datas['message_text']) >= 5) {
+        $formErrors['min5car'] = false;
+    } else {
+        $formErrors['min5car'] = 'Minimum 5 caractères';
+    }
 
     // Verifier si le genre renseigné est "Mr" ou "Mme"
-    $isGenderValid = ($datas['gender'] === "Mr") || ($datas['gender'] === "Mme");
+    if (($datas['gender'] === "Mr") || ($datas['gender'] === "Mme")) {
+        $formErrors['gender'] = false;
+    } else {
+        $formErrors['gender'] = "Le genre doit être 'Mr' ou 'Mme'";
+    }
 
     // Verifier si le sujet renseigné est "proposition emploi" ou "demande information et prestations"
-    $isSubjectValid = ($datas['contact_subject'] === "proposition emploi") || ($datas['contact_subject'] === "demande information et prestations");
+    if (($datas['contact_subject'] === "proposition emploi") || ($datas['contact_subject'] === "demande information et prestations")) {
+        $formErrors['contact_subject'] = false;
+    } else {
+        $formErrors['contact_subject'] = "Le sujet doit être 'proposition emploi' ou 'demande information et prestations'";
+    }
 
     // Enregistrement dans le fichier texte dans le dossier /contact sous la forme "contact_2020-01-12-09-52-00.txt"
-    if ($isClean && !$isLastnameEmpty && !$isFirstnameEmpty && !$isEmailEmpty && !$isMessageEmpty && $isEmailValid && $isMin5car && $isGenderValid && $isSubjectValid) {
+    if ($isClean
+        && !$formErrors['gender']
+        && !$formErrors['contact_lastname']
+        && !$formErrors['contact_firstname']
+        && !$formErrors['contact_email']
+        && !$formErrors['message_text']
+        && !$formErrors['contact_email_valid']
+        && !$formErrors['min5car']
+        && !$formErrors['contact_subject']) {
         // Envoyer les données dans le fichier texte
         // On écrit le chemin du fichier
         $file = 'contact/contact_' . date('Y-m-d-H-i-s') . '.txt';
         // Ecriture du fichier
         if (file_put_contents($file, implode(' - ', $datas))) {
             // Message d'information - Tout s'est bien passé
-            $msg_info = "Votre message à bien été envoyé";
+            $formInfos['msg_info'] = 'Les informations ont bien été enregistrées';
             // On vide le formulaire
             $datas['contact_lastname'] = '';
             $datas['contact_firstname'] = '';
             $datas['contact_email'] = '';
             $datas['message_text'] = '';
         } else {
-            $msg_info = "Erreur d'écriture du fichier";
+            $formInfos['msg_info'] = 'Erreur d\'écriture du fichier';
         }
     } else {
         // Message d'information - Erreur dans les données
-        $msg_info = "Des données du formulaire à envoyé ne sont pas valide";
+        $formInfos['msg_info'] = 'Des données du formulaire à envoyé ne sont pas valide';
     }
 }
 ?>
@@ -84,9 +138,9 @@ if ($formSent) {
         <div id="content">
             <article class="main_style transparence">
                 <h3 class="sub_title">Formulaire de Contact</h3>
-                <p class="error_message"><?= isset($msg_info) ? $msg_info : '' ?></p>
+                <p class="error_message"><?= isset($formInfos['msg_info']) ? $formInfos['msg_info'] : '' ?></p>
                 <form action="?page=contact" method="POST">
-                    <p class="error_message"><?= (isset($datas['gender']) && !$isGenderValid) ? $datas['gender'] . 'n\'est pas une donnée valide petit malin^^' : '' ?></p>
+                    <p class="error_message"><?= (isset($formErrors['gender']) && $formErrors['gender']) ? $formErrors['gender'] : '' ?></p>
                     <div id="select_list">
                         <label for="gender">Genre : </label>
                         <select name="gender" id="gender">
@@ -94,28 +148,28 @@ if ($formSent) {
                             <option value="Mme">Madame</option>
                         </select>
                     </div>
-                    <p class="error_message"><?= (isset($isLastnameEmpty) && $isLastnameEmpty) ? 'Champ nom vide' : '' ?></p>
+                    <p class="error_message"><?= (isset($formErrors['contact_lastname']) && $formErrors['contact_lastname']) ? $formErrors['contact_lastname'] : '' ?></p>
                     <div class="champs">
                         <label for="contact_lastname">Votre Nom : </label>
                         <input type="text" id="contact_lastname" name="contact_lastname"
                                placeholder="Tapez votre nom ici..."
                                value="<?= isset($datas['contact_lastname']) ? $datas['contact_lastname'] : '' ?>">
                     </div>
-                    <p class="error_message"><?= (isset($isFirstnameEmpty) && $isFirstnameEmpty) ? 'Champ prénom vide' : '' ?></p>
+                    <p class="error_message"><?= (isset($formErrors['contact_firstname']) && $formErrors['contact_firstname']) ? $formErrors['contact_firstname'] : '' ?></p>
                     <div class="champs">
                         <label for="contact_firstname">Votre Prénom : </label>
                         <input type="text" id="contact_firstname" name="contact_firstname"
                                placeholder="Tapez votre Prénom..."
                                value="<?= isset($datas['contact_firstname']) ? $datas['contact_firstname'] : '' ?>">
                     </div>
-                    <p class="error_message"><?= (isset($isEmailEmpty) && $isEmailEmpty) ? 'Champ email vide' : '' ?></p>
-                    <p class="error_message"><?= (isset($isEmailValid) && !$isEmailValid) ? 'Email non valide' : '' ?></p>
+                    <p class="error_message"><?= (isset($formErrors['contact_email']) && $formErrors['contact_email']) ? $formErrors['contact_email'] : '' ?></p>
+                    <p class="error_message"><?= (isset($formErrors['contact_email_valid']) && $formErrors['contact_email_valid']) ? $formErrors['contact_email_valid'] : '' ?></p>
                     <div class="champs">
                         <label for="contact_email">Votre adresse mail : </label>
                         <input type="text" id="contact_email" name="contact_email"
                                placeholder="Tapez votre adresse email ici...">
                     </div>
-                    <p class="error_message"><?= (isset($datas['contact_subject']) && !$isSubjectValid) ? $datas['contact_subject'] . 'n\'est pas une donnée valide petit malin^^' : '' ?></p>
+                    <p class="error_message"><?= (isset($formErrors['contact_subject']) && $formErrors['contact_subject']) ? $formErrors['contact_subject'] : '' ?></p>
                     <div id="radio_buttons">
                         <label>raison du contact :</label>
                         <input type="radio" id="emploi" name="contact_subject" value="proposition emploi" checked>
@@ -124,8 +178,8 @@ if ($formSent) {
                                value="demande information et prestations">
                         <label for="info_prestations">Demande d'information et prestations</label>
                     </div>
-                    <p class="error_message"><?= (isset($isMessageEmpty) && $isMessageEmpty) ? 'Champ prénom vide' : '' ?></p>
-                    <p class="error_message"><?= (isset($isMin5car) && !$isMin5car) ? 'Le message doit comporter minimum 5 caractères' : '' ?></p>
+                    <p class="error_message"><?= (isset($formErrors['message_text']) && $formErrors['message_text']) ? $formErrors['message_text'] : '' ?></p>
+                    <p class="error_message"><?= (isset($formErrors['min5car']) && $formErrors['min5car']) ? $formErrors['min5car'] : '' ?></p>
                     <div id="message">
                         <label for="message_text">Votre message :</label>
                         <textarea name="message_text" id="message_text" cols="50"
