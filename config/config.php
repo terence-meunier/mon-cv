d535ce6669e370a00e97cf482b7c183a1c27063c
Térence MEUNIER <terence.meunier@gmail.com>
2021-01-14 13:57:36 +0100

    feat(config): Add configuration files with routes, titles and description

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+// Routes
+$routes = [
+    'profil' => 'pages' . DIRECTORY_SEPARATOR . 'profil.php',
+    'hobby' => 'pages' . DIRECTORY_SEPARATOR . 'hobby.php',
+    'contact' => 'pages' . DIRECTORY_SEPARATOR . 'contact.php',
+    'error404' => 'error' . DIRECTORY_SEPARATOR . '404.php'
+];
+
+// Titre
+$titles = [
+    'profil' => 'Page profil',
+    'hobby' => 'Page hobby',
+    'contact' => 'Page Contact',
+    'error404' => 'Page erreur 404'
+];
+
+// Description
+$descriptions = [
+    'profil' => 'Description profil',
+    'hobby' => 'Description hobby',
+    'contact' => 'Description Contact',
+    'error404' => 'Description erreur 404'
+];
